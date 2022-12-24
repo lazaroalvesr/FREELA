d968efa24fdf25ec7d5854fa6a2a683142ac02c0
Lazaro Alves <lazaroalves12355@gmail.com>
2022-12-24 11:48:16 -0300

    first commit

---
+++ videos.js
@@ -0,0 +1,42 @@
+export default [
+    {
+      src: "./video/video2.mp4",
+      title: "Auto Bomba para Concreto",
+    },
+    {
+      src: "./video/vide.mp4",
+      title: "Auto Bomba para Concreto",
+    },
+    {
+      src: "./video/03.mp4",
+      title: "Video",
+    },
+    {
+      src: "./video/04.mp4",
+      title: "Video",
+    },
+    {
+      src: "./video/05.mp4",
+      title: "Video",
+    },
+    {
+      src: "./video/06.mp4",
+      title: "Video",
+    },
+    {
+      src: "./video/07.mp4",
+      title: "Video",
+    },
+    {
+        src: "./video/08.mp4",
+        title: "Video",
+      },
+      {
+        src: "./video/09.mp4",
+        title: "Video",
+      },
+      {
+        src: "./video/10.mp4",
+        title: "Video",
+      },
+  ];
